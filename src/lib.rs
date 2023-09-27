bb5d3962321ac4d24ff9925ca67056f938b7d551
Defmc <defmc0101@gmail.com>
2023-09-27 10:07:22 -0400

    fix: remove `std` from `impl_allocator`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(allocator_api)]
 #![feature(pointer_is_aligned)]
+#![no_std]
 
 use core::{
     alloc::Layout,
@@ -23,12 +24,6 @@ impl AlloqMetaData {
     }
 
     pub unsafe fn write_meta(&self, ptr: *mut u8) -> *mut u8 {
-        println!(
-            "{:?} -> {} .:. {}",
-            ptr,
-            ptr as usize,
-            ptr as usize % core::mem::size_of::<Self>()
-        );
         *(ptr as *mut AlloqMetaData) = *self;
         ptr.offset(core::mem::size_of::<AlloqMetaData>() as isize)
     }
@@ -70,13 +65,16 @@ pub trait Alloqator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8;
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout);
 
-    fn allocate(&self, layout: Layout) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+    fn allocate(
+        &self,
+        layout: Layout,
+    ) -> Result<core::ptr::NonNull<[u8]>, core::alloc::AllocError> {
         let p = unsafe { self.alloc(layout) };
         let slice = unsafe { core::slice::from_raw_parts_mut(p, layout.size()) };
         Ok(NonNull::new(slice).expect("oh null pointer"))
     }
 
-    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: std::alloc::Layout) {
+    unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: core::alloc::Layout) {
         self.dealloc(ptr.as_ptr(), layout);
     }
 }
@@ -84,25 +82,25 @@ pub trait Alloqator {
 #[macro_export]
 macro_rules! impl_allocator {
     ($typ:ty) => {
-        unsafe impl std::alloc::Allocator for $typ {
+        unsafe impl core::alloc::Allocator for $typ {
             fn allocate(
                 &self,
-                layout: std::alloc::Layout,
-            ) -> Result<std::ptr::NonNull<[u8]>, std::alloc::AllocError> {
+                layout: core::alloc::Layout,
+            ) -> Result<core::ptr::NonNull<[u8]>, core::alloc::AllocError> {
                 Alloqator::allocate(self, layout)
             }
 
-            unsafe fn deallocate(&self, ptr: std::ptr::NonNull<u8>, layout: std::alloc::Layout) {
+            unsafe fn deallocate(&self, ptr: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
                 Alloqator::deallocate(self, ptr, layout)
             }
         }
 
-        unsafe impl std::alloc::GlobalAlloc for Alloq {
-            unsafe fn alloc(&self, layout: std::alloc::Layout) -> *mut u8 {
+        unsafe impl core::alloc::GlobalAlloc for Alloq {
+            unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
                 Alloqator::alloc(self, layout)
             }
 
-            unsafe fn dealloc(&self, ptr: *mut u8, layout: std::alloc::Layout) {
+            unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
                 Alloqator::dealloc(self, ptr, layout)
             }
         }
