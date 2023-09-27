2ff155310e730fd4b96da6da3a01a3209fe06d3b
Defmc <defmc0101@gmail.com>
2023-09-27 11:59:59 -0400

    fix: move `crate::AlloqMetaData` to `list::AlloqMetaData`

--- src/lib.rs
+++ src/lib.rs
@@ -13,28 +13,6 @@ pub mod list;
 //#[cfg(feature = "bump")]
 pub mod bump;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct AlloqMetaData {
-    size: usize,
-}
-
-impl AlloqMetaData {
-    pub fn new(size: usize) -> Self {
-        Self { size }
-    }
-
-    pub unsafe fn write_meta(&self, ptr: *mut u8) -> *mut u8 {
-        *(ptr as *mut AlloqMetaData) = *self;
-        ptr.offset(core::mem::size_of::<AlloqMetaData>() as isize)
-    }
-
-    pub const fn total_size(&self) -> usize {
-        self.size + core::mem::size_of::<AlloqMetaData>()
-    }
-
-    pub unsafe fn from_ptr(ptr: *const u8) -> AlloqMetaData {
-        *(ptr as *const AlloqMetaData)
-    }
 }
 
 pub trait Alloqator {
--- src/list.rs
+++ src/list.rs
@@ -1,7 +1,30 @@
-use crate::{AlloqMetaData, Alloqator};
+use crate::Alloqator;
 use core::ops::Range;
-use core::slice;
+use core::{mem, slice};
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct AlloqMetaData {
+    size: usize,
+}
+
+impl AlloqMetaData {
+    pub fn new(size: usize) -> Self {
+        Self { size }
+    }
+
+    pub unsafe fn write_meta(&self, ptr: *mut u8) -> *mut u8 {
+        *(ptr as *mut AlloqMetaData) = *self;
+        ptr.offset(core::mem::size_of::<AlloqMetaData>() as isize)
+    }
+
+    pub const fn total_size(&self) -> usize {
+        self.size + core::mem::size_of::<AlloqMetaData>()
+    }
+
+    pub unsafe fn from_ptr(ptr: *const u8) -> AlloqMetaData {
+        *(ptr as *const AlloqMetaData)
+    }
+}
 pub struct Alloq {
     heap_start: *const u8,
     heap_end: *const u8,
